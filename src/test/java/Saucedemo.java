d5a0db9ef64a092e2c23c76d1892a01de3eabf59
frenkco <austinkhwinana23@gmail.com>
2025-03-06 14:50:14 +0200

    valid login details

--- src/test/java/Saucedemo.java
+++ src/test/java/Saucedemo.java
@@ -1,2 +1,23 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.Test;
+
 public class Saucedemo {
+    WebDriver driver;
+
+    @Test
+public void loginInWithValidDetails(){
+    driver= new ChromeDriver();
+    driver.get("https://www.saucedemo.com/");
+    driver.findElement(By.id("user-name")).sendKeys("standard_user");
+    driver.findElement(By.id("password")).sendKeys("secret_sauce");
+    driver.findElement(By.id("login-button")).click();
+
+}
+//@AfterTest
+    public void closeBrowser(){
+        driver.quit();
+}
 }
